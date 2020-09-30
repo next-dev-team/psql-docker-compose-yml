f48d054c529121db09c10232b11eb634a917eaad
Moun Chhaily <moun.chhaily@sabay.com>
2020-09-30 10:46:38 +0700

    fix setup for master and testing

--- docker/scripts/mongo_setup-testing.sh
+++ docker/scripts/mongo_setup-testing.sh
@@ -11,19 +11,27 @@ mongo --host mongo1.testing.sabay.com:27017 <<EOF
       {
         "_id": 0,
         "host": "mongo1.testing.sabay.com:27017",
-        "priority": 2
+        "priority": 1
       },
       {
         "_id": 1,
         "host": "mongo2.testing.sabay.com:27017",
-        "priority": 0
+        "priority": 0.5
       },
       {
         "_id": 2,
         "host": "mongo3.testing.sabay.com:27017",
-        "priority": 0
+        "priority": 0.5
       }
     ]
   };
   rs.initiate(cfg);
+EOF
+
+mongo --host mongo2.testing.sabay.com:27017 <<EOF
+  rs.slaveOk();
+EOF
+
+mongo --host mongo3.testing.sabay.com:27017 <<EOF
+  rs.slaveOk();
 EOF
